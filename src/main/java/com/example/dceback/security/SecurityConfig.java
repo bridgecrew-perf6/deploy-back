4a1396b316370a499cbfecf7a6ffd8dd35a13ba5
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-30 19:01:32 -0300

    .

--- src/main/java/com/example/dceback/security/SecurityConfig.java
+++ src/main/java/com/example/dceback/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELET", "OPTIONS"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
